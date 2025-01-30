ba6f7be15f9285fbd88c716a27741bfae41e82dd
insu <qkrdlstn9701@naver.com>
2025-01-31 03:32:55 +0900

    test

--- airflow/models/book_mlops/main.py
+++ airflow/models/book_mlops/main.py
@@ -3,15 +3,6 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
-from support.config import TTBKEY
-from models.book_mlops.data.db_update import db_update
-from models.book_mlops.data.save_books_to_database import (
-    save_books_to_database_task
-)
-from models.book_mlops.data.data_extract import search_and_collect_books
-from models.book_mlops.data.json_to_db import json_to_db
-from models.book_mlops.model.new_model_create import new_model_create
-from models.book_mlops.model.predict import predict
 
 import sys
 import os
@@ -25,6 +16,16 @@ sys.path.append(os.path.join(BASE_DIR, "support"))  # support 모듈 추가
 sys.path.append(os.path.join(BASE_DIR, "models"))  # models 모듈 추가
 sys.path.append(os.path.join(BASE_DIR, "models", "book_mlops"))  # book_mlops 추가
 sys.path.append(os.path.join(BASE_DIR, "models", "book_recommend"))  # book_recommend 추가
+
+from support.config import TTBKEY
+from models.book_mlops.data.db_update import db_update
+from models.book_mlops.data.save_books_to_database import (
+    save_books_to_database_task
+)
+from models.book_mlops.data.data_extract import search_and_collect_books
+from models.book_mlops.data.json_to_db import json_to_db
+from models.book_mlops.model.new_model_create import new_model_create
+from models.book_mlops.model.predict import predict
 
 local_timezone = pendulum.timezone('Asia/Seoul')
 
