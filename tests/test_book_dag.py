19c0f2cdc73d361028bb175ddf63748d628ba29d
insu <qkrdlstn9701@naver.com>
2025-01-31 02:48:37 +0900

    test

--- tests/test_book_dag.py
+++ tests/test_book_dag.py
@@ -3,7 +3,7 @@ from airflow.models import DagBag
 
 class TestBookDag(unittest.TestCase):
     def test_dag_loaded(self):
-        dagbag = DagBag(dag_folder='/home/insu/airflow/dags/models/book_mlops/', include_examples=False)
+        dagbag = DagBag(dag_folder='/home/insu/airflow/dags/models/book_mlops/main.py', include_examples=False)
         dag = dagbag.get_dag(dag_id='book_mlops')
         self.assertIsNotNone(dag)
         self.assertEqual(len(dagbag.import_errors), 0)
