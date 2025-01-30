6cecf00935d5e6bad74a2a63013ea67cb55ee16b
insu <qkrdlstn9701@naver.com>
2025-01-31 03:07:30 +0900

    test

--- tests/test_book_dag.py
+++ tests/test_book_dag.py
@@ -1,13 +1,13 @@
-import unittest
+import os
+import sys
 from airflow.models import DagBag
 
-class TestBookDag(unittest.TestCase):
-    def test_dag_loaded(self):
-        dagbag = DagBag(dag_folder='/home/runner/work/mlops-book/mlops-book/airflow/models/main.py', include_examples=False)
-        dag = dagbag.get_dag(dag_id='book_mlops')
-        self.assertIsNotNone(dag)
-        self.assertEqual(len(dagbag.import_errors), 0)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-if __name__ == '__main__':
-    unittest.main()
+class TestBookDag:
+    def test_dag_loaded(self):
+        dagbag = DagBag(dag_folder=os.environ.get('AIRFLOW__CORE__DAGS_FOLDER', ''), include_examples=False)
+        dag = dagbag.get_dag('book_mlops')
+        assert dag is not None
+        assert len(dagbag.import_errors) == 0, f"DAG import errors: {dagbag.import_errors}"
 
